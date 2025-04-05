b6dd058fdc944553593771bf3e14dd34e3474298
jeswin <jeswinpeter92@gmail.com>
2025-04-05 15:04:56 +0530

    Adding complete FIFO Page replacement

--- FIFo.c
+++ FIFo.c
@@ -3,56 +3,63 @@
 int main() {
     int p_request;
     int p_frames;
-    int hits;
-    int fault;
+    int hits = 0;
+    int fault = 0;
+    int j = 0;
 
     printf("Enter the no. of page request: ");
     scanf("%d",&p_request);
     printf("Enter the no. of page frames: ");
     scanf("%d",&p_frames);
-    printf("Enter the no. of page request: ");
 
-    int frames[p_frames], request[p_request];
+    int frames[p_frames], req[p_request];
 
     printf("Enter page requests: ");    
     for(int i = 0; i < p_request; i++) {
-        scanf("%d",&request[i]);
-        if(i < p_frames) {
-            frames[i] = -1;
-        }
+        scanf("%d",&req[i]);
     }
 
-    printf("------- Page Repacement ---------");
+    for(int a = 0; a < p_frames; a++) {
+        frames[a] = -1;
+    }
+
+    printf("------ Page Repacement ------\n");
 
     for(int i = 0; i < p_request; i++) {
         int avail = 0;
 
-        printf("%d : ", request[i]);
+        printf("\t%d : ", req[i]);
         
         for(int a = 0; a < p_frames; a++) {
-            if(frames[a] = request[i]) {
+            if(frames[a] == req[i]) {
                 avail = 1;
                 hits++;
                 break;
             }
         }
 
-        if(avail = 0) {
-            frames[j] = request[i];
+        if(avail == 0) {
+            frames[j] = req[i];
             j = (j + 1) % p_frames;
             fault++;
         }   
 
-        if(avail = 1) {
-            printf("- Hit -");
+        if(avail == 1) {
+            printf("- Hit -\n");
         }
         else {
             for(int a = 0; a < p_frames; a++) {
-                if(frames[a] == -1) {
-                    printf(" - ")
+                if(frames[a] == -1) {       //Empty frames
+                    printf("- ");
+                }
+                else {
+                    printf("%d ",frames[a]);
                 }
             }
+            printf("\n");
         }
     }
 
+    printf("------------------------------\nNumber of page faults -> %d\n",fault);
+    printf("Number of hits -> %d\n",hits);
 }
