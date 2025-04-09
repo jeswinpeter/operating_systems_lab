e746e5f2960e6268bcbee7578ad514e6fed9280e
jeswin <jeswinpeter92@gmail.com>
2025-04-09 22:26:12 +0530

    Modified

--- round_robin.c
+++ round_robin.c
@@ -1,59 +1,84 @@
-#include<stdio.h> 
-
-
-
+#include<stdio.h>
 
 void main() {
-	int n, t_quant, flag = 0;
-	int total = 0; // total time
-	int time_index = 0; // Track number of entries in Gantt chart
-	float avgwt = 0, avgtat = 0;
-	
-	int time_line[100];
-	
-	printf("Enter the number of processes: ");
-	scanf("%d", &n);
-	
-	struct process {
-		int id, Bt, Wt, Tat , tBt;
-	} p[n];
-	
-	for (int i = 0; i < n; i++) {
-	
-		printf("Enter the process ID and Burst Time of P-%d: ",i)
-		scanf("%d%d", &p[i].id, &p[i].Bt);
-		p[i].tBt = p[i].Bt;
-		p[i].Wt = 0;
-		p[i].TAt = 0;
+	int n_proce;
+	int quant;
+	int avg_tat = 0;
+	int avg_wt = 0;
+
+	printf("Enter no. of Processes: ");
+	scanf("%d",&n_proce);
+	printf("Enter Time Quantum: ");
+	scanf("%d",&quant);
+	// printf("Enter ");
+
+	int b_time[n_proce];
+	int Remain_time[n_proce];
+	int wait_t[n_proce];
+	int TAT[n_proce];
+	int gantt[100];
+	int gant_time[100];
+	int done = 0;
+	int time = 0;
+	int index = 0;
+
+	printf("Enter Burst times of process: ");
+	for(int i = 0; i < n_proce; i++) {
+		scanf("%d",&b_time[i]);
+		Remain_time[i] = b_time[i];
+		wait_t[i] = 0;
 	}
-	
-	printf("\nEnter the time quantum: ");
-	scanf("%d",&t_quant);
-	
-	while(flag != n) {
-		for(int i = 0; i < n; i++) {
-			if(p[i].tBt > t_quant) {
-				for(int j = 0; j < t_quant; j++) {
-					timel_line[time_index++] = p[i].id
+
+	while(!done) {
+		done = 1;
+		
+		for(int i = 0; i < n_proce; i++) {
+			if(Remain_time[i] > 0) {
+				done = 0;
+				gantt[index] = i;
+				gant_time[index++] = time;
+				if(Remain_time[i] > quant) {
+					time += quant;
+					Remain_time[i] -= quant;
 				}
-				total += t_quant;
-				p[i].tBt -= t_quant;
-			}
-			else if(p[i].rt > 0) {
-				for(int j = 0; j < p[i].tBt; j++) {
-					time_line[time_index++] = p[i].id;
+				else{
+					time += Remain_time[i];
+					Remain_time[i] = 0;
+					wait_t[i] = time - b_time[i];
 				}
-				total += p[i].tBt;
-				p[i].tBt = 0;
-				p[i].Tat = total
-				p[i].Wt = total - p[i].bt;
-				flag++;
+				
 			}
 		}
 	}
-	
-	printf("\nProcess ID\tBurst Time\tWaiting Time\tTurn Around Time");
-	for() {
-		
+
+	gant_time[index] = time;
+
+	for(int i = 0; i < n_proce; i++) {
+		TAT[i] = wait_t[i] - b_time[i];
+		avg_tat += TAT[i];
+		avg_wt += wait_t[i]; 
 	}
+
+	for(int i = 0 ; i < n_proce; i++) {
+		printf("\n P[%d]\t%d\t%d\t%d\t",i + 1, b_time[i], wait_t[i], TAT[i]);
+	}
+
+	printf("\nGantt Chart\n");
+	for(int i = 0; i < index; i++) {
+		printf(" ------ ");
+	}
+	printf("\n");
+	for(int i = 0; i < index; i++) {
+		printf("| P[%d]\t",gantt[i] + 1);
+	}
+	printf("|\n");
+	
+	for(int i = 0; i < index; i++) {
+		printf(" ------ ");
+	}
+	printf("\n");
+	for(int i = 0; i <= index; i++) {
+		printf("%d\t",gant_time[i]);
+	}
+	printf("\n");
 }
